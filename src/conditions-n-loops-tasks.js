5e6eb73c22dd1fd946e25d90620d47d0ecbea389
artemgassan <artemgassan@ya.ru>
2024-12-23 22:01:13 +0300

    feat: finish convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -158,8 +158,65 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let result = '';
+  let index = 0;
+
+  while (index < numberStr.length) {
+    const char = numberStr[index];
+    let word = '';
+
+    switch (char) {
+      case '0':
+        word = 'zero';
+        break;
+      case '1':
+        word = 'one';
+        break;
+      case '2':
+        word = 'two';
+        break;
+      case '3':
+        word = 'three';
+        break;
+      case '4':
+        word = 'four';
+        break;
+      case '5':
+        word = 'five';
+        break;
+      case '6':
+        word = 'six';
+        break;
+      case '7':
+        word = 'seven';
+        break;
+      case '8':
+        word = 'eight';
+        break;
+      case '9':
+        word = 'nine';
+        break;
+      case '.':
+      case ',':
+        word = 'point';
+        break;
+      case '-':
+        word = 'minus';
+        break;
+      default:
+        throw new Error(`Unsupported character: ${char}`);
+    }
+
+    if (result !== '') {
+      result += ' ';
+    }
+    result += word;
+
+    index += 1;
+  }
+
+  return result;
 }
 
 /**
